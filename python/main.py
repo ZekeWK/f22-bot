d9325f75f7d691a2f40d39296a3a57ee4ed87935
Elias Lundell <elilun03@gmail.com>
2022-11-09 10:45:46 +0100

    Move gitigonre

---
+++ python/main.py
@@ -0,0 +1,11 @@
+from mattermostdriver import Driver
+
+def main():
+    driver = Driver({
+        'url': 'mattermost.fysiksektionen.se',
+        'token': 
+        })
+    pass
+
+if __name__ == "__main__":
+    main()
