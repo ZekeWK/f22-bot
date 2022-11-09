3168645d2613fe82f5cf527c387921be8a4ed770
Elias Lundell <elilun03@gmail.com>
2022-11-09 11:02:07 +0100

    Use port 443

--- python/main.py
+++ python/main.py
@@ -5,6 +5,8 @@ from secret import TOKEN
 def main():
     driver = Driver({
         'url': 'mattermost.fysiksektionen.se',
+        'scheme': 'https',
+        'port': 443,
         'token': TOKEN,
         'keepalive': True,
         'keepalive_delay': 5
