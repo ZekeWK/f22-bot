02f4119341375d04305153deafed689c6bf82da4
William Kraft <william.z.kraft@gmail.com>
2022-11-10 12:56:33 +0100

    Add Proper json

--- python/test.py
+++ python/test.py
@@ -1,12 +1,14 @@
 from asyncio import run
-import websockets
+import websockets.client
 from secret import TOKEN
+import json
 
-to_send = " { \"seq\": 1, \"action\": \"authentication_challenge\", \"data\": { \"token\": \"" + TOKEN + "\" } } "
+
+to_send = json.dumps({ "seq": 1, "action": "authentication_challenge", "data": { "token": TOKEN}})
 
 async def main():
     uri = "wss://mattermost.fysiksektionen.se:443/api/v4/websocket"
-    async with websockets.connect(uri) as websocket:
+    async with websockets.client.connect(uri) as websocket:
         await websocket.send(to_send)
         while True:
             print(await websocket.recv())
