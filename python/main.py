667d068e5e641fe04583d522529a27f217495863
Elias Lundell <elilun03@gmail.com>
2022-11-10 14:16:45 +0100

    Start of logic

--- python/main.py
+++ python/main.py
@@ -1,8 +1,15 @@
 from mattermostdriver import Driver
+from threading import Thread
 import json
 
 from secret import TOKEN
 from configuration import *
+
+def remove_channel_member(driver, channel_id, user_id):
+    driver.channels.remove_channel_member(channel_id, user_id)
+
+def add_channel_member(driver, channel_id, user_id):
+    driver.channels.add_user(channel_id, {"user_id": user_id})
 
 def reactions_update(driver: Driver):
     reactions = driver.reactions.get_reactions_of_post(post_id = COURSE_REACTIONS_POST_ID)
@@ -18,7 +25,27 @@ def reactions_update(driver: Driver):
         if reaction["emoji_name"] == "triangular_ruler":
             users[reaction["user_id"]] |= COURSES["math"]
 
-    print(users)
+    threads = []
+    users_in_channels = {}
+    for channel in COURSE_CHANNEL_IDS:
+        thread = users_in_channels[channel] = driver.channels.get_channel_members(COURSE_CHANNEL_IDS[channel])
+
+    for channel in COURSE_CHANNEL_IDS:
+        for user_in_channel in users_in_channels[channel]:
+            if user_in_channel["user_id"] not in users or channel not in users[user_in_channel["user_id"]]:
+                if user_in_channel["user_id"] == driver.client.userid:
+                    continue
+                print(f"Removing '{user_in_channel['user_id']}' from '{channel}'")
+                Thread(target = remove_channel_member, args = (driver, COURSE_CHANNEL_IDS[channel], user_in_channel["user_id"])).start()
+            else:
+                users[user_in_channel["user_id"]] -= { channel }
+
+    for user in users:
+        if not users[user]:
+            continue
+        print(f"Adding '{user}' to '{users[user]}'")
+        for channel in users[user]:
+            Thread(target = add_channel_member, args = (driver, COURSE_CHANNEL_IDS[channel], user)).start()
 
 async def event(data):
     print(data)
