70b5509c172f45961b37d2fc2404cc5652ff2cca
Elias Lundell <elilun03@gmail.com>
2022-11-11 11:53:42 +0100

    Manage categories

--- python/main.py
+++ python/main.py
@@ -6,23 +6,62 @@ from secret import TOKEN
 from configuration import *
 from ws import WebSocket
 
-def remove_channel_member(driver, channel, user_id, users_in_channels):
+
+def mm_channels_create_user_sidebar_category(driver: Driver, user_id, team_id, options=None):
+    return driver.client.post(
+        '/users/' + user_id + '/teams/' + team_id + '/channels/categories',
+        options=options
+    )
+
+def mm_channels_get_user_sidebar_categories(driver: Driver, user_id, team_id):
+    return driver.client.get(
+        '/users/' + user_id + '/teams/' + team_id + '/channels/categories'
+    )
+
+def mm_channels_update_user_sidebar_categories(driver: Driver, user_id, team_id, options=None):
+    return driver.client.get(
+        '/users/' + user_id + '/teams/' + team_id + '/channels/categories',
+        options=options
+    )
+
+def mm_channels_delete_user_sidebar_category(driver: Driver, user_id, team_id, category_id):
+    return driver.client.delete(
+        '/users/' + user_id + '/teams/' + team_id + '/channels/categories/' + category_id
+    )
+
+def remove_channel_member(driver: Driver, channel, user_id, users_in_channels):
     driver.channels.remove_channel_member(COURSE_CHANNEL_IDS[channel], user_id)
     users_in_channels[channel].remove(user_id)
 
-def add_channel_member(driver, channel, user_id, users_in_channels):
+def add_channel_member(driver: Driver, channel, user_id, users_in_channels):
     driver.channels.add_user(COURSE_CHANNEL_IDS[channel], {"user_id": user_id})
     users_in_channels[channel].add(user_id)
 
-def get_channel_members(driver, channel, channel_id, users_in_channels):
+def get_channel_members(driver: Driver, channel, channel_id, users_in_channels):
     users_in_channels[channel] = { user["user_id"] for user in driver.channels.get_channel_members(channel_id) }
 
-def manage_channel_categories(user_id):
+def manage_channel_categories(driver: Driver, user_id, team_id, users_in_channels):
     # make sure this happens AFTER the user has been added to the relevant categories
-    # get user channel categories
-    # move the kurser ones
-    # send request
-    pass
+    categories = mm_channels_get_user_sidebar_categories(driver, user_id, team_id)["categories"]
+
+    channel_ids = [COURSE_CHANNEL_IDS[channel] for channel in COURSE_CHANNEL_IDS]
+
+    course_category_name = "Kurser"
+
+    for category in categories:
+        if category["display_name"] == course_category_name:
+            mm_channels_delete_user_sidebar_category(driver, user_id, team_id, category["id"])
+
+    if any(user_id in users_in_channels[channel] for channel in users_in_channels):
+        mm_channels_create_user_sidebar_category(driver, user_id, team_id, 
+                                                 { 
+                                                  "user_id": user_id, 
+                                                  "team_id": team_id, 
+                                                  "display_name": course_category_name, 
+                                                  "type": "custom", 
+                                                  "channel_ids": channel_ids
+                                                  }
+                                                 )
 
 class CourseChannels:
     def __init__(self, driver: Driver):
@@ -34,13 +73,6 @@ class CourseChannels:
         self.setup_reactions_and_users_in_channels()
         self.build_users()
         self.fix_diff()
-
-        print("react")
-        print(self.reactions)
-        print("us in ch")
-        print(self.users_in_channels)
-        print("users")
-        print(self.users)
 
     def setup_reactions_and_users_in_channels(self):
         self.reactions = {}
@@ -73,13 +105,16 @@ class CourseChannels:
                     self.users[user] |= COURSES["math"]
 
     def fix_diff(self):
+        threads = []
         for channel in COURSE_CHANNEL_IDS:
             for user_in_channel in self.users_in_channels[channel]:
                 if user_in_channel not in self.users or channel not in self.users[user_in_channel]:
                     if user_in_channel == self.driver.client.userid:
                         continue
                     print(f"Removing '{user_in_channel}' from '{channel}'")
-                    Thread(target = remove_channel_member, args = (self.driver, channel, user_in_channel, self.users_in_channels)).start()
+                    thread = Thread(target = remove_channel_member, args = (self.driver, channel, user_in_channel, self.users_in_channels))
+                    thread.start()
+                    threads.append(thread)
                 else:
                     self.users[user_in_channel] -= { channel }
 
@@ -88,7 +123,15 @@ class CourseChannels:
                 continue
             print(f"Adding '{user}' to '{self.users[user]}'")
             for channel in self.users[user]:
-                Thread(target = add_channel_member, args = (self.driver, channel, user, self.users_in_channels)).start()
+                thread = Thread(target = add_channel_member, args = (self.driver, channel, user, self.users_in_channels))
+                thread.start()
+                threads.append(thread)
+
+        for thread in threads:
+            thread.join()
+
+        for user in self.users:
+            manage_channel_categories(self.driver, user, TEAM_ID, self.users_in_channels)
 
     def reaction_added(self, data):
         reaction = json.loads(data["reaction"])
@@ -112,35 +155,27 @@ class CourseChannels:
         self.fix_diff()
 
 def main():
-    driver = Driver({
-        'url': 'mattermost.fysiksektionen.se',
-        'basepath': '/api/v4',
-        'verify': True,
-        'scheme': 'https',
-        'port': 443,
-        'auth': None,
-        'token': TOKEN,
-        'keepalive': True,
-        'keepalive_delay': 5,
-        })
+    driver = Driver(
+            {
+                'url': 'mattermost.fysiksektionen.se',
+                'basepath': '/api/v4',
+                'verify': True,
+                'scheme': 'https',
+                'port': 443,
+                'auth': None,
+                'token': TOKEN,
+                'keepalive': True,
+                'keepalive_delay': 5,
+                }
+            )
 
     driver.login()
-
-    driver.channels
-
-
-#    print(driver.teams.get_team(team_id=TEAM_ID))
-
     ws = WebSocket()
 
     cc = CourseChannels(driver)
 
     ws.subscribe("reaction_added", cc.reaction_added)
     ws.subscribe("reaction_removed", cc.reaction_removed)
-
-#    driver.init_websocket(event)
-
-    #driver.disconnect()
 
 if __name__ == "__main__":
     main()
