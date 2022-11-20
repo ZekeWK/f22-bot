28bec59acb24008dae91b62b2e0e814e71983b9c
LogFlames <elilun03@gmail.com>
2022-11-20 14:33:00 +0100

    New conf

--- python/configuration.py
+++ python/configuration.py
@@ -1,15 +1,15 @@
 TEAM_ID = "yw6pexns4p8yzbhoohthh9z45c"
-COURSE_REACTIONS_POST_ID = "ozke5upm1ibhdxe5ptxkbjq9ba"
+COURSE_REACTIONS_POST_ID = "itnb1umirjd8dkyx7da11i5dhy"
 
 COURSE_CHANNEL_IDS = {
-        "sf1672_linalg": "o8jhd3i9bjfo8rzo5d9ohboyfw",
-        "sf1673_envarre": "p5j1wba4d3fz7kxzecpmbhaw6r",
-        "sf1918_sanstat": "9akkosqc8ir6xk186e4fbm9kww",
-        "sk1104_klasfys": "kawddezi8pnjj8mpea6z7qfe6r",
-        "si1121_termo": "mt3o9ahatpgquezno1nh9pbtka",
-        "dd1301_datintro": "xb1jijco7ifoz8h5ca73ccfhgw",
-        "dd1331_gruprog": "e9iban6fm7bktpyztjdxnu3hxc",
-        "sa1006_ingen": "hzjcizarmbgb5xu79d6asc3q4r"
+        "sf1672_linalg": "hqnrfcxaqpda7kk3ya3gk95one",
+        "sf1673_envarre": "s69kjmhywtg59nzhu6z1pqq7ir",
+        "sf1918_sanstat": "jn1cg6dwg7gmuqhsoizuoa1k4w",
+        "sk1104_klasfys": "wrfw5qisyjghdd3p345x4owhmh",
+        "si1121_termo": "xpodofigiidkfq6cszjx61xgmo",
+        "dd1301_datintro": "3e8nsaikmjbifpb7rdjafhgntw",
+        "dd1331_gruprog": "3pe5jdzyity3dmihaxm3m6eo3r",
+        "sa1006_ingen": "rfofcaowh7dczd5gn9i3c8eb9h"
         }
 
 COURSES = {
@@ -27,9 +27,9 @@ COURSES = {
         }
 
 DEFAULT_CHANNELS = {
-        "kurser": "3m7k48qih3rmdyghqi3f761z3w"
+        "kurser": "itnb1umirjd8dkyx7da11i5dhy"
         }
 
 CLEAN_ADDED_TO_MESSAGES_CHANNELS = {
-        "kurser": "3m7k48qih3rmdyghqi3f761z3w"
+        "kurser": "itnb1umirjd8dkyx7da11i5dhy"
         }
