97b6990d6dd0827ce442a5028e41ed1bd2866e5d
Elias Lundell <elilun03@gmail.com>
2023-01-17 17:11:43 +0100

    Add new courses

--- python/configuration.py
+++ python/configuration.py
@@ -4,11 +4,15 @@ COURSE_REACTIONS_POST_ID = "itnb1umirjd8dkyx7da11i5dhy"
 COURSE_CHANNEL_IDS = {
         "sf1672_linalg": "hqnrfcxaqpda7kk3ya3gk95one",
         "sf1673_envarre": "s69kjmhywtg59nzhu6z1pqq7ir",
+        "sf1674_flervarre": "uqfj73bjk38npj98ce67sdikxy",
         "sf1918_sanstat": "jn1cg6dwg7gmuqhsoizuoa1k4w",
+        "sf1550_numme": "qzc3jguo6jf85ggwnxujxijd9e",
         "sk1104_klasfys": "wrfw5qisyjghdd3p345x4owhmh",
         "si1121_termo": "xpodofigiidkfq6cszjx61xgmo",
+        "sg1112_mekaniki": "htjc9hro6fdr9jyhoa7sodxhuc",
         "dd1301_datintro": "3e8nsaikmjbifpb7rdjafhgntw",
         "dd1331_gruprog": "3pe5jdzyity3dmihaxm3m6eo3r",
+        "dd1320_tillämpad_datalogi": "az7c9e863frz7dwamim1eqktro",
         "sa1006_ingen": "rfofcaowh7dczd5gn9i3c8eb9h"
         }
 
@@ -16,13 +20,18 @@ COURSES = {
         "math": {
             "sf1672_linalg", 
             "sf1918_sanstat", 
-            "sa1006_ingen"
+            "sa1006_ingen",
+            "sf1674_flervarre",
+            "sf1550_numme",
+            "dd1320_tillämpad_datalogi"
             },
         "physics": {
             "sf1672_linalg", 
             "sk1104_klasfys", 
             "dd1331_gruprog", 
-            "si1121_termo"
+            "si1121_termo",
+            "sf1674_flervarre",
+            "sg1112_mekaniki"
             }
         }
 
