afe78b3e10267f5a8241ac8cf8aa8bf88e743873
William Kraft <william.z.kraft@gmail.com>
2023-03-20 11:18:45 +0100

    Add channels for period 3

--- python/configuration.py
+++ python/configuration.py
@@ -13,19 +13,24 @@ COURSE_CHANNEL_IDS = {
         "dd1301_datintro": "3e8nsaikmjbifpb7rdjafhgntw",
         "dd1331_gruprog": "3pe5jdzyity3dmihaxm3m6eo3r",
         "dd1320_tillämpad_datalogi": "az7c9e863frz7dwamim1eqktro",
-        "sa1006_ingen": "rfofcaowh7dczd5gn9i3c8eb9h"
+        "sa1006_ingen": "rfofcaowh7dczd5gn9i3c8eb9h",
+        "dd1396_parprog" : "a1e8iu5mc7rbmqxd5xsc6wc3br",
+        "sg1115_partikel_dyn" : "agmgmeq7t3yi8edbjgdkg9uamh",
+        "sk1105_exp_fysik" : "ns4erkj7yjfsucwjtkfa5m58to",
+        "sf1922_sanstat" : "ooskfqdfg3ndppfxrn6gf37poa"
         }
 
 COURSES = {
         "math": {
-            "sf1674_flervarre",
             "sf1550_numme",
-            "dd1320_tillämpad_datalogi"
+            "dd1320_tillämpad_datalogi",
+            "sg1115_partikel_dyn",
+            "dd1396_parprog",
             },
         "physics": {
-            "sk1104_klasfys", 
-            "sf1674_flervarre",
-            "sg1112_mekaniki"
+            "sg1112_mekaniki",
+            "sk1105_exp_fysik",
+            "sf1922_sanstat",
             },
         "archive": {
             "sf1672_linalg",
@@ -34,6 +39,8 @@ COURSES = {
             "dd1331_gruprog",
             "si1121_termo",
             "sf1673_envarre"
+            "sf1674_flervarre",
+            "sk1104_klasfys", 
             }
         }
 
