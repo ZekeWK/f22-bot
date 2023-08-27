a02d74dca97d7bb97c2f74abaa68d377c69cd0ef
William Kraft <william.z.kraft@gmail.com>
2023-08-27 23:00:54 +0200

    Update courses

--- python/configuration.py
+++ python/configuration.py
@@ -17,20 +17,27 @@ COURSE_CHANNEL_IDS = {
         "dd1396_parprog" : "a1e8iu5mc7rbmqxd5xsc6wc3br",
         "sg1115_partikel_dyn" : "agmgmeq7t3yi8edbjgdkg9uamh",
         "sk1105_exp_fysik" : "ns4erkj7yjfsucwjtkfa5m58to",
-        "sf1922_sanstat" : "ooskfqdfg3ndppfxrn6gf37poa"
+        "sf1922_sanstat" : "ooskfqdfg3ndppfxrn6gf37poa",
+        "sf1692_ode" : "6m1uhx8fpiygzd9c7gyg7n54hc",
+        "sf1930_statdat" : "ops7uhj8j3na8pzepni4rtfeze",
+        "dd1385_progut" : "khzwh5t7n7g4zr5nqfu3k1p9hr",
+        "dd1380_java" : "mkduqwkt3jd3fyda5afkd6pr9c",
+        "sg1113_mek_fk" : "7rn4fsd48pnb3q6zb14kxdpypo",
+        "si1146_vektor" : "1aeeyq7wttyrfc6aznmwser8qo",
+        "sf1683_difftrans" : "hg4eucooub85bxio7rjjd4q8iw",
         }
 
 COURSES = {
         "math": {
-            "sf1550_numme",
-            "dd1320_tillämpad_datalogi",
-            "sg1115_partikel_dyn",
-            "dd1396_parprog",
+            "sf1692_ode",
+            "sf1930_statdat",
+            "dd1385_progut",
+            "dd1380_java", 
             },
         "physics": {
-            "sg1112_mekaniki",
-            "sk1105_exp_fysik",
-            "sf1922_sanstat",
+            "sg1113_mek_fk",
+            "si1146_vektor",
+            "sf1683_difftrans",
             },
         "archive": {
             "sf1672_linalg",
@@ -41,6 +48,13 @@ COURSES = {
             "sf1673_envarre",
             "sf1674_flervarre",
             "sk1104_klasfys", 
+            "sf1550_numme",
+            "dd1320_tillämpad_datalogi",
+            "sg1115_partikel_dyn",
+            "dd1396_parprog",
+            "sg1112_mekaniki",
+            "sk1105_exp_fysik",
+            "sf1922_sanstat",
             }
         }
 
